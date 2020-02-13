550167755f953f1df4c66e999f04c780c687fd41
waseem <waseemafzallqp@yahoo.com>
2020-02-12 16:55:13 -0800

    config

--- application/config/config.php
+++ application/config/config.php
@@ -26,7 +26,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 //$config['base_url'] = 'http://localhost/skillsquared.com/';
 
 
-$config['base_url'] = 'http://localhost/education/';
+$config['base_url'] = 'http://localhost/cpxglobal/';
 
 
 /*
